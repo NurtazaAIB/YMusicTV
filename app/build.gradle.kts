3269a74504216f8a25f8ff88ee26a3a0ebc00ddd
NurtazaAIB <commoditiesaib@gmail.com>
2026-09-18 02:47:36 +0500

    Bump app to 0.9.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "dev.ymusictv"
         minSdk = 28
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.9.8.1"
+        versionCode = 24
+        versionName = "0.9.8.2"
     }
 
     compileOptions {

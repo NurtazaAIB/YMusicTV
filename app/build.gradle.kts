818a3d64679467081447462884739aceb722b600
NurtazaAIB <commoditiesaib@gmail.com>
2026-09-18 00:46:52 +0500

    Bump app to 0.9.5.4 modern Wave test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "dev.ymusictv"
         minSdk = 28
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.9.5.3"
+        versionCode = 17
+        versionName = "0.9.5.4"
     }
 
     compileOptions {

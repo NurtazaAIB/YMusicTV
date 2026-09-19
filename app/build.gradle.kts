77c737b61d61eda1bb4ecf033b71da349bb7af49
NurtazaAIB <commoditiesaib@gmail.com>
2026-09-19 21:59:13 +0500

    Version 0.10.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "dev.ymusictv"
         minSdk = 28
         targetSdk = 35
-        versionCode = 38
-        versionName = "0.10.5"
+        versionCode = 39
+        versionName = "0.10.6"
     }
 
     compileOptions {

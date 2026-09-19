5964a98bbe559e10419727743de59cdd69fdbb19
NurtazaAIB <commoditiesaib@gmail.com>
2026-09-19 21:02:16 +0500

    Fix mini player focus import

--- app/src/main/java/dev/ymusictv/MainActivity.kt
+++ app/src/main/java/dev/ymusictv/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.onPreviewKeyEvent
